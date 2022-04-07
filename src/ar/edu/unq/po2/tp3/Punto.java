63a303738b9893582c7980a2b2b19adcb3c3ab91
JuanCruz <juancruzinsaurralde@outlook.com>
2022-04-07 16:42:13 -0300

    version inicial

---
+++ src/ar/edu/unq/po2/tp3/Punto.java
@@ -0,0 +1,27 @@
+package ar.edu.unq.po2.tp3;
+
+public class Punto {
+    private float x;
+    private float y;
+    
+	public Punto(float x, float y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+	
+	float getX() {
+		return x;
+	}
+	void setX(float x) {
+		this.x = x;
+	}
+	float getY() {
+		return y;
+	}
+	void setY(float y) {
+		this.y = y;
+	}
+    
+    
+}
